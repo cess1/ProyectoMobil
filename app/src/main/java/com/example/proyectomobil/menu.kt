c3794593c4df2b43ba0045b9d853c4c9fe269cbb
DiegoLaRealeza <diego5gtt@gmail.com>
2023-11-13 23:18:21 -0600

    menu

---
+++ app/src/main/java/com/example/proyectomobil/menu.kt
@@ -0,0 +1,15 @@
+package com.example.proyectomobil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu : AppCompatActivity() {
+
+
+    //hola
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
