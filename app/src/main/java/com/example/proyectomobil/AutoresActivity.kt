e4a4b837ce3aff828c93025a8edc0de3d55b1a38
franciscb4 <franciscobulnes04@gmail.com>
2023-11-26 22:19:46 -0500

    avance principal

---
+++ app/src/main/java/com/example/proyectomobil/AutoresActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyectomobil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AutoresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_autores)
+    }
+}
--- app/src/main/java/com/example/proyectomobil/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectomobil/PrincipalActivity.kt
@@ -1,11 +1,38 @@
 package com.example.proyectomobil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        var btnAcceder : Button = findViewById(R.id.btnAcceder)
+        var btnRegistrar : Button = findViewById(R.id.btnRegistrar)
+        var btnSalir : Button = findViewById(R.id.btnSalir)
+        var btnAutores : Button = findViewById(R.id.btnAutores)
+
+        btnAcceder.setOnClickListener {
+            val accederScreen = Intent(this, AccederActivity::class.java)
+            startActivity(accederScreen)
+        }
+
+        btnRegistrar.setOnClickListener {
+            val registrarScreen = Intent(this, RegistrarActivity::class.java)
+            startActivity(registrarScreen)
+        }
+
+        btnSalir.setOnClickListener {
+
+        }
+
+        btnAutores.setOnClickListener {
+            val autoresScreen = Intent(this, AutoresActivity::class.java)
+            startActivity(autoresScreen)
+        }
     }
 }
