2947f3f741f9f8d04cbe01c0912bbe4f1ea50f1e
Cesar Camacho <cescamacho94@gmail.com>
2023-11-27 20:47:38 -0500

    Se creo Registrar Usuario

--- app/src/main/java/com/example/proyectomobil/RegistrarActivity.kt
+++ app/src/main/java/com/example/proyectomobil/RegistrarActivity.kt
@@ -2,10 +2,44 @@ package com.example.proyectomobil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.proyectomobil.SQLite.BDHelper
 
 class RegistrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
+
+        val btnGrabar : Button = findViewById(R.id.btnGrabarUsuario)
+
+        btnGrabar.setOnClickListener {
+            val inputCorreo : EditText = findViewById(R.id.txtCorreo)
+            val inputNombres : EditText = findViewById(R.id.txtNombres)
+            val inputApellidos : EditText = findViewById(R.id.txtApellidos)
+            val inputTelefono : EditText = findViewById(R.id.txtTelefono)
+            val inputContrasena : EditText = findViewById(R.id.txtContraseña)
+
+
+            val correo = inputCorreo.text.toString()
+            val nombres = inputNombres.text.toString()
+            val apellidos = inputApellidos.text.toString()
+            val telefono = inputTelefono.text.toString()
+            val contrasena = inputContrasena.text.toString()
+
+            val db = BDHelper(this, null)
+
+            db.CrearRegistro(correo, nombres, apellidos,telefono, contrasena )
+            Toast.makeText(this, "Se registro el usuario de manera exitosa", Toast.LENGTH_LONG).show()
+
+            inputCorreo.text.clear()
+            inputNombres.text.clear()
+            inputApellidos.text.clear()
+            inputTelefono.text.clear()
+            inputContrasena.text.clear()
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/proyectomobil/SQLite/BDHelper.kt
@@ -0,0 +1,61 @@
+package com.example.proyectomobil.SQLite
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDHelper (context: Context, factory: SQLiteDatabase.CursorFactory?)
+    : SQLiteOpenHelper (context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+        companion object {
+            private val DATABASE_NAME = "BD_Mapapin"
+            private val DATABASE_VERSION = 1
+            private val TABLA_USUARIOS = "USUARIO"
+            private val COLUMN_ID = "IDUSER"
+            private val COLUMN_NOMBRES = "NOMBRES"
+            private val COLUMN_APELLIDOS = "APELLIDOS"
+            private val COLUMN_TELEFONO = "TELEFONO"
+            private val COLUMN_CORREO = "CORREO"
+            private val COLUMN_CONTRASENA = "CONTRASEÑA"
+
+        }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        var queryCreateTable =
+            ("CREATE TABLE " + TABLA_USUARIOS + " ( " +
+                    COLUMN_ID + " INT PRIMARY KEY, " +
+                    COLUMN_NOMBRES + "TEXT, " +
+                    COLUMN_APELLIDOS + "TEXT, " +
+                    COLUMN_TELEFONO + "INT, "+
+                    COLUMN_CORREO + "TEXT, "+
+                    COLUMN_CONTRASENA + "TEXT" + ")"
+                    )
+        db.execSQL(queryCreateTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun CrearRegistro(
+        correo:String, nombres:String,
+        apellidos:String, telefono: String, contrasena:String ){
+    val values = ContentValues();
+        values.put(COLUMN_NOMBRES,nombres)
+        values.put(COLUMN_APELLIDOS,apellidos)
+        values.put(COLUMN_TELEFONO,telefono)
+        values.put(COLUMN_CORREO,correo)
+        values.put(COLUMN_CONTRASENA,contrasena)
+        val db = this.writableDatabase
+        db.insert(TABLA_USUARIOS, null, values)
+        db.close()
+    }
+    fun Acceder(telefono:Int, contrasena: String) : Cursor? {
+        val db = this.readableDatabase
+        val sql = "SELECT * FROM " + TABLA_USUARIOS + " WHERE " + COLUMN_TELEFONO + " = '" + telefono + "' AND " +
+                COLUMN_CONTRASENA + " = '" + contrasena + "' "
+        return db.rawQuery(sql, null)
+    }
+}
